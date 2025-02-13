d76615dc1aa08e490d636184359c46a2ddea891b
Cell6969 <bossmarinoo@gmail.com>
2025-02-13 16:36:20 +0700

    add: view for product in home

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from . models import Category, Product
 
 # Create your views here.
 
+@require_GET
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+
+    context = {'products': products}
+    return render(request, 'store/store.html', context=context)
+
+def categories(request):
+    categories_all = Category.objects.all()
+    return {"categories": categories_all} 
