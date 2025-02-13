3afcc8bb24e9610e4fef55edf6e4ff0899cccb0d
Cell6969 <bossmarinoo@gmail.com>
2025-02-13 19:20:17 +0700

    add: dynamic url for product info

--- ecom/store/models.py
+++ ecom/store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -40,3 +41,6 @@ class Product (models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('product-info', args=[self.slug])
